cd2ef23d65e47cfb164d98cd264d0c8bd4ed37d5
auki <auki@simcorp.com>
2024-11-22 17:30:35 -0300

    add some setup

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # advent-of-code-2024
+
 Solutions to the Advent Of Code 2024 by some of the Simcorp Buenos Aires Devs
+
+
+## How to run the problems
+
+To run all solutions execute without any parameter:
+
+```powershell
+> .\run.ps1
+```
+
+To run for a specific solution like "mySolution" and a day execute:
+
+```powershell
+> .\run.ps1 -solution mySolution -day 8
+```
+
+To configure a project and install its dependencies run:
+
+```powershell
+> .\run.ps1 -solution mySolution -setup
+```
+
+## How to add a new solution
+
+Copy the template solution `solutions/template` 
+
+```powershell
+cp -Recursive solutions/template solutions/mySolution
+```
+
+Then you have to populate three files, which are self explanatory:
+
+- [setup.ps1](./solutions/template/setup.ps1)
+- [build.ps1](./solutions/template/build.ps1)
+- [run.ps1](./solutions/template/run.ps1)
+
+You can do whatever you what within your solution directory, but be considerate with the rest
---
+++ run.ps1
@@ -0,0 +1,59 @@
+param(
+    [int]$day,
+    [string]$solution,
+    [switch]$setup
+)
+
+function Resolve-InputPath {
+    param(
+        [int]$day
+    )
+    Resolve-Path (Join-Path $PSScriptRoot problems ("day-{0:D2}" -f $day) input.txt)
+}
+
+function Invoke-Solution {    
+    param(
+        [string]$solution,
+        [int]$day)
+
+    if ($day -eq 0){
+        $days = 1..31
+    } else {
+        $days = $($day) 
+    }    
+
+    $result = $days | %{ . $solution $_ (Resolve-InputPath $_)}
+    $result
+}
+
+
+$solutions = Get-ChildItem (Join-Path $PSScriptRoot solutions)
+$solutionPath = (Join-Path $PSScriptRoot solutions $solution)
+
+$solExists = Test-Path -Path $solutionPath -PathType Container
+
+if (-not [string]::IsNullOrEmpty($solution) -and $solExists){
+    $solutions = $( $solutionPath )
+}elseif (-not [string]::IsNullOrEmpty($solution) -and -not $solExists){
+    throw "Solution '$solutionPath' does not exist."
+}
+
+if ($setup){
+    foreach ($solution in $solutions) {
+        $build = (Join-Path $solution setup.ps1)
+        . $setup
+    }
+    Write-Host "Completed setup"
+    return
+}
+$results = @{}
+
+foreach ($solution in $solutions) {
+    $build = (Join-Path $solution build.ps1)
+    $runPath = (Join-Path $solution run.ps1)
+    . $build
+    $solutionResults = Invoke-Solution $runPath $day
+    $results.add($solution, $solutionResults)
+}
+
+$results
