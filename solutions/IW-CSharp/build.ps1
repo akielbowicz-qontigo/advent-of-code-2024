0e369890de4020f2fdbe93562a373dc4c1dd639a
Ignacio Wiman <iwiman@qontigo.com>
2024-11-29 12:08:04 -0300

    Populate setup and build files

--- solutions/IW-CSharp/build.ps1
+++ solutions/IW-CSharp/build.ps1
@@ -1,3 +1,5 @@
 Write-Host "Building $PSScriptRoot"
 
 # Put all commands to build your project
+
+dotnet build --configuration Release --output $PSScriptRoot\AdventOfCode2024\bin
--- solutions/IW-CSharp/setup.ps1
+++ solutions/IW-CSharp/setup.ps1
@@ -1,1 +1,7 @@
 ### Add all setup needed for your project
+
+# Install .NET SDK
+scoop install main/dotnet-sdk
+
+# Install extension for C# development in VSCode
+code --install-extension ms-dotnettools.csharp
