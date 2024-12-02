b02609c550921f6191ab15aa293391a757056aa9
Ignacio Wiman <108736719+iwiman-qontigo@users.noreply.github.com>
2024-12-02 09:21:22 -0300

    (IW) Setup solution (#3)

---
+++ solutions/IW-CSharp/run.ps1
@@ -0,0 +1,9 @@
+param(
+    [int]$day,
+    [string]$inputAbsolutePath
+)
+# Put your command to execute here
+# it should receive an integer indicating the day to run and the problem input absolute path
+# $result = adventOfCode.exe 1 "$inputAbsolutePath"
+$result = 0
+return $result
