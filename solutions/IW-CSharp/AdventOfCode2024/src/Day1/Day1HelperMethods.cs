53163deb786d198b3cfa4d797bacb7e8e774a98f
Ignacio Wiman <iwiman@qontigo.com>
2024-12-03 12:10:49 -0300

    Refactor

--- solutions/IW-CSharp/AdventOfCode2024/src/Day1/Day1HelperMethods.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day1/Day1HelperMethods.cs
@@ -3,10 +3,32 @@
     public static class Day1HelperMethods
     {
         /// <summary>
+        /// Given a set of lines with two numbers separated by at least one space, this method returns all the left numbers 
+        /// in a list and all the right numbers in another list.<br></br>
+        /// For example, given the two lines "11 22" and "33 44", this method will return a list containing 11 and 33, and another 
+        /// list containing 22 and 44.
+        /// </summary>
+        public static (List<int>, List<int>) SaveInputInLists(string filePath)
+        {
+            var leftList = new List<int>();
+            var rightList = new List<int>();
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var leftNumber = GetLeftNumberFromLine(line);
+                leftList.Add(leftNumber);
+
+                var rightNumber = GetRightNumberFromLine(line);
+                rightList.Add(rightNumber);
+            }
+
+            return (leftList, rightList);
+        }
+
+        /// <summary>
         /// Given a line with two numbers separated by at least one space (or any non-digit character), this method returns the number in the left.<br></br>
         /// For example, given the line "123 456", this method will return 123.
         /// </summary>
-        public static int GetLeftNumberFromLine(string line)
+        private static int GetLeftNumberFromLine(string line)
         {
             string number = "";
             foreach (char c in line)
@@ -28,7 +50,7 @@
         /// Given a line with two numbers separated by at least one space (or any non-digit character), this method returns the number in the right.<br></br>
         /// For example, given the line "123 456", this method will return 456.
         /// </summary>
-        public static int GetRightNumberFromLine(string line)
+        private static int GetRightNumberFromLine(string line)
         {
             var reversedLine = line.Reverse();
             string reversedNumber = "";
--- solutions/IW-CSharp/AdventOfCode2024/src/Day1/Day1Part1.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day1/Day1Part1.cs
@@ -5,16 +5,7 @@
         public static double Day1Part1Main(string filePath)
         {
             // Save all the left and right numbers into two lists.
-            var leftList = new List<int>();
-            var rightList = new List<int>();
-            foreach (string line in File.ReadLines(filePath))
-            {
-                var leftNumber = Day1HelperMethods.GetLeftNumberFromLine(line);
-                leftList.Add(leftNumber);
-
-                var rightNumber = Day1HelperMethods.GetRightNumberFromLine(line);
-                rightList.Add(rightNumber);
-            }
+            var (leftList, rightList) = Day1HelperMethods.SaveInputInLists(filePath);
 
             // Sort the two lists, calculate the differences between the elements and sum.
             leftList.Sort();
