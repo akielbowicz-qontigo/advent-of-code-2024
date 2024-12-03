f39e94beadea9fa82ba30047cab73bbc2bfe3daf
Ignacio Wiman <iwiman@qontigo.com>
2024-12-03 15:34:42 -0300

    Finish part 1

--- solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part1.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part1.cs
@@ -1,7 +1,66 @@
 ﻿namespace AdventOfCode2024
 {
-    public class Day2Part1
+    public static class Day2Part1
     {
+        public static double Day2Part1Main(string filePath)
+        {
+            var safeReportCount = 0;
 
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var report = line.Split(' ').Select(n => int.Parse(n)).ToList();
+                if (report.IsSafe())
+                {
+                    safeReportCount++;
+                }
+            }
+
+            return safeReportCount;
+        }
+
+        private static bool IsSafe(this List<int> report)
+        {
+            // Check strictly monotone condition.
+            if (!report.LevelsAreDecreasing() && !report.LevelsAreIncreasing())
+            {
+                return false;
+            }
+
+            // Check maximun change condition. The minimum change condition is redundant at this point so we don't check it.
+            for (var i = 1; i < report.Count; i++)
+            {
+                if (Math.Abs(report[i] - report[i-1]) > 3)
+                {
+                    return false;
+                }
+            }
+
+            // If we made it here, it means the report is safe.
+            return true;
+        }
+
+        private static bool LevelsAreDecreasing(this List<int> report)
+        {
+            for (var i = 1; i < report.Count; i++)
+            {
+                if (report[i] >= report[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool LevelsAreIncreasing(this List<int> report)
+        {
+            for (var i = 1; i < report.Count; i++)
+            {
+                if (report[i] <= report[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- solutions/IW-CSharp/AdventOfCode2024/tests/Day1/Day1Part1Tests.cs
+++ solutions/IW-CSharp/AdventOfCode2024/tests/Day1/Day1Part1Tests.cs
@@ -5,17 +5,9 @@ namespace AdventOfCode2024
     public class Day1Part1Tests
     {
         [Test]
-        public void SmallSampleTest()
-        {
-            var filePath = "C:/Dev/AOC-2024/problems/day-01/input-part1-sample.txt";
-            var result = Day1Part1.Day1Part1Main(filePath);
-            Assert.That(result, Is.EqualTo(11));
-        }
-
-        [Test]
         public void FullInputTest()
         {
-            var filePath = "C:/Dev/AOC-2024/problems/day-01/input-part1-full.txt";
+            var filePath = "C:/Dev/AOC-2024/problems/day-01/input.txt";
             var result = Day1Part1.Day1Part1Main(filePath);
             Assert.That(result, Is.EqualTo(2031679));
         }
