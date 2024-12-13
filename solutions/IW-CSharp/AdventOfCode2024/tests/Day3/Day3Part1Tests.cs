5f369494b932e1b7f385645a78e879756b8609eb
Ignacio Wiman <iwiman@qontigo.com>
2024-12-13 16:01:57 -0300

    Add files for Day3

---
+++ solutions/IW-CSharp/AdventOfCode2024/tests/Day3/Day3Part1Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2024
+{
+    public class Day3Part1Tests
+    {
+        [Test]
+        public void FullInputTest()
+        {
+            var filePath = "C:/Dev/AOC-2024/problems/day-03/input.txt";
+            var result = Day3Part1.Day3Part1Main(filePath);
+            Assert.That(result, Is.EqualTo(2031679));
+        }
+    }
+}
---
+++ solutions/IW-CSharp/AdventOfCode2024/tests/Day3/Day3Part2Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2024
+{
+    public class Day3Part2Tests
+    {
+        [Test]
+        public void FullInputTest()
+        {
+            var filePath = "C:/Dev/AOC-2024/problems/day-03/input.txt";
+            var result = Day3Part2.Day3Part2Main(filePath);
+            Assert.That(result, Is.EqualTo(2031679));
+        }
+    }
+}
