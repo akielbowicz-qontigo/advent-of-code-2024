83ce96a7b706465d406cffe38a29d0b8d6eecefe
Ignacio Wiman <iwiman@qontigo.com>
2024-12-13 15:57:34 -0300

    Finish part 2

--- solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part1.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part1.cs
@@ -17,50 +17,5 @@
 
             return safeReportCount;
         }
-
-        private static bool IsSafe(this List<int> report)
-        {
-            // Check strictly monotone condition.
-            if (!report.LevelsAreDecreasing() && !report.LevelsAreIncreasing())
-            {
-                return false;
-            }
-
-            // Check maximun change condition. The minimum change condition is redundant at this point so we don't check it.
-            for (var i = 1; i < report.Count; i++)
-            {
-                if (Math.Abs(report[i] - report[i-1]) > 3)
-                {
-                    return false;
-                }
-            }
-
-            // If we made it here, it means the report is safe.
-            return true;
-        }
-
-        private static bool LevelsAreDecreasing(this List<int> report)
-        {
-            for (var i = 1; i < report.Count; i++)
-            {
-                if (report[i] >= report[i - 1])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private static bool LevelsAreIncreasing(this List<int> report)
-        {
-            for (var i = 1; i < report.Count; i++)
-            {
-                if (report[i] <= report[i - 1])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
--- solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part2.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day2/Day2Part2.cs
@@ -1,7 +1,40 @@
 ﻿namespace AdventOfCode2024
 {
-    public class Day2Part2
+    public static class Day2Part2
     {
+        public static double Day2Part2Main(string filePath)
+        {
+            var safeReportCount = 0;
 
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var report = line.Split(' ').Select(n => int.Parse(n)).ToList();
+                if (report.IsSafeWithProblemDampener())
+                {
+                    safeReportCount++;
+                }
+            }
+
+            return safeReportCount;
+        }
+
+        private static bool IsSafeWithProblemDampener(this List<int> report)
+        {
+            if (report.IsSafe()) return true;
+
+            // If the report is not directly safe, we try to remove one element at a time and see if we can make it safe this way.
+            for (var i = 0; i < report.Count; i++)
+            {
+                var tempReport = new List<int>(report);  // Copy by value to avoid modifying the original report!
+                tempReport.RemoveAt(i);
+                if (tempReport.IsSafe())
+                {
+                    return true;
+                }
+            }
+
+            // If we made it here, it means the report is not safe and removing one element from it won't make it safe either.
+            return false;
+        }
     }
 }
