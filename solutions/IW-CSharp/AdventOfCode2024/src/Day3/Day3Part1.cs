c5c05c2cacc0bb0c1fe181878834df167fc30bbd
Ignacio Wiman <108736719+iwiman-qontigo@users.noreply.github.com>
2024-12-18 11:12:43 -0300

    (IW) Day 3 (#6)

--- solutions/IW-CSharp/AdventOfCode2024/src/Day3/Day3Part1.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day3/Day3Part1.cs
@@ -1,10 +1,29 @@
-﻿namespace AdventOfCode2024
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024
 {
     public static class Day3Part1
     {
         public static double Day3Part1Main(string filePath)
         {
-            throw new NotImplementedException();
+            var total = 0;
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var searchPattern = @"(mul)\((\d+),(\d+)\)";
+                MatchCollection matches = Regex.Matches(line, searchPattern);
+
+                var currentTotal = 0;
+                foreach (Match match in matches)
+                {
+                    var first = int.Parse(match.Groups[2].Value);
+                    var second = int.Parse(match.Groups[3].Value);
+                    currentTotal += first * second;
+                }
+
+                total += currentTotal;
+            }
+
+            return total;
         }
     }
 }
--- solutions/IW-CSharp/AdventOfCode2024/src/Day3/Day3Part2.cs
+++ solutions/IW-CSharp/AdventOfCode2024/src/Day3/Day3Part2.cs
@@ -1,10 +1,43 @@
-﻿namespace AdventOfCode2024
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024
 {
     public static class Day3Part2
     {
         public static double Day3Part2Main(string filePath)
         {
-            throw new NotImplementedException();
+            var total = 0;
+            var isEnabled = true;
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var searchPattern = @"(mul)\((\d+),(\d+)\)|do\(\)|don't\(\)";
+                MatchCollection matches = Regex.Matches(line, searchPattern);
+
+                var currentTotal = 0;
+                foreach (Match match in matches)
+                {
+                    isEnabled = SetIsEnabled(match.Value, isEnabled);
+
+                    if (isEnabled == true && match.Groups[1].Value == "mul")
+                    {
+                        var first = int.Parse(match.Groups[2].Value);
+                        var second = int.Parse(match.Groups[3].Value);
+                        currentTotal += first * second;
+                    }
+                    
+                }
+
+                total += currentTotal;
+            }
+
+            return total;
+        }
+
+        private static bool SetIsEnabled(string command, bool currentState)
+        {
+            if (command == "do()") return true;
+            if (command == "don't()") return false;
+            return currentState;
         }
     }
 }
